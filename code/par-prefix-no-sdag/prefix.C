7dc8772353b21828e410fcaf37549f37f3287e91
Bilge Acun <acun2@illinois.edu>
2013-09-10 01:45:49 -0500

    another bug fix

--- code/par-prefix-no-sdag/prefix.C
+++ code/par-prefix-no-sdag/prefix.C
@@ -42,37 +42,40 @@ struct Prefix : public CBase_Prefix {
   Prefix(CkMigrateMessage*){};
 
   void step(int value){
-    int sendIndex = thisIndex + (1<<stage);
-    if(sendIndex < numElements)
-      thisProxy[sendIndex].passValue(stage, value);
-    if(thisIndex-(1<<stage) < 0){
-      stage++;
-      if(stage < numStages)
-        step(value);              
-    }
     if(stage >= numStages){
       CkPrintf("Prefix[%d].value = %d\n", thisIndex, value);
       contribute(CkCallback(CkReductionTarget(Main, done), mainProxy));
     }
+    else{
+      int sendIndex = thisIndex + (1<<stage);
+      if(sendIndex < numElements)
+        thisProxy[sendIndex].passValue(stage, value);
+      //if you no longer receive, but need to continue sending
+      if(thisIndex-(1<<stage) < 0){
+        stage++;
+        step(value);              
+      }
+    }
   }
 
   void passValue(int incoming_stage, int incoming_value){
-    CkPrintf("[%d] get value from stage [%d]\n", thisIndex, incoming_stage);
+    //CkPrintf("[%d] get value from stage [%d]\n", thisIndex, incoming_stage);
     buffer[incoming_stage] = incoming_value;
+
+    //if the data is coming from a higher stage, set the flag
     if(incoming_stage > stage) flag = 1;
-    else if(numStages > stage){
+    else{
       value += buffer[incoming_stage];
       stage++;
       step(value);
       if(flag == 1){
-      CkPrintf("flag 1.\n");
         for(int i=stage; i<numStages; i++){
             if(buffer[i] == 0) break; 
             value += buffer[i];
             stage++;
             step(value);
+            flag = 0;
         }
-        flag = 0;
       }
     }
   }
